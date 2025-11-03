23bc6eab1c9c63cbe83a0755ddd29ecd67838efc
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-03 19:09:30 +0100

    step: add infra adapters

---
+++ infra/adapters/index.ts
@@ -0,0 +1,6 @@
+export * from "./certificate-inspector.adapter";
+export * from "./clock.adapter";
+export * from "./disk-space-checker.adapter";
+export * from "./id-provider.adapter";
+export * from "./json-file-reader.adapter";
+export * from "./logger.adapter";
---
+++ infra/adapters/logger.adapter.ts
@@ -0,0 +1,27 @@
+import * as bg from "@bgord/bun";
+import { Env } from "+infra/env";
+
+const app = "journal";
+
+const redactor = new bg.RedactorCompositeAdapter([
+  new bg.RedactorMaskAdapter(bg.RedactorMaskAdapter.DEFAULT_KEYS),
+  new bg.RedactorCompactArrayAdapter(),
+  new bg.RedactorCompactObjectAdapter(),
+]);
+
+export const LoggerWinstonLocalAdapter = new bg.LoggerWinstonLocalAdapter({ app, redactor }).create(
+  Env.LOGS_LEVEL,
+);
+
+export const LoggerWinstonProductionAdapter = new bg.LoggerWinstonProductionAdapter({
+  app,
+  AXIOM_API_TOKEN: Env.AXIOM_API_TOKEN,
+  redactor,
+});
+
+export const Logger: bg.LoggerPort = {
+  [bg.NodeEnvironmentEnum.local]: LoggerWinstonLocalAdapter,
+  [bg.NodeEnvironmentEnum.test]: new bg.LoggerNoopAdapter(),
+  [bg.NodeEnvironmentEnum.staging]: new bg.LoggerNoopAdapter(),
+  [bg.NodeEnvironmentEnum.production]: LoggerWinstonProductionAdapter.create(Env.LOGS_LEVEL),
+}[Env.type];
