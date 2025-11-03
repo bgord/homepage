c6f19889268dbdb5a8699038d26dfb0d760e3c20
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-03 19:14:51 +0100

    step: setup error handler

---
+++ app/http/error-handler.ts
@@ -0,0 +1,78 @@
+import * as bg from "@bgord/bun";
+import * as tools from "@bgord/tools";
+import type hono from "hono";
+import { HTTPException } from "hono/http-exception";
+import z from "zod/v4";
+import { Logger } from "+infra/adapters";
+
+export class ErrorHandler {
+  static handle: hono.ErrorHandler = async (error, c) => {
+    const url = c.req.url;
+    const correlationId = c.get("requestId") as bg.CorrelationIdType;
+
+    if (error instanceof HTTPException) {
+      if (error.message === "request_timeout_error") {
+        return c.json({ message: "request_timeout_error", _known: true }, 408);
+      }
+
+      if (error.message === bg.AccessDeniedApiKeyError.message) {
+        return c.json(
+          { message: bg.AccessDeniedApiKeyError.message, _known: true },
+          bg.AccessDeniedApiKeyError.status,
+        );
+      }
+
+      if (error.message === bg.AccessDeniedAuthShieldError.message) {
+        return c.json({ message: bg.AccessDeniedAuthShieldError.message, _known: true }, 403);
+      }
+
+      if (error.message === bg.TooManyRequestsError.message) {
+        return c.json(
+          { message: bg.TooManyRequestsError.message, _known: true },
+          bg.TooManyRequestsError.status,
+        );
+      }
+
+      return error.getResponse();
+    }
+
+    if (error.message === tools.MimeValueError.Invalid) {
+      return c.json({ message: "invalid.mime", _known: true }, 400);
+    }
+
+    if (error.message === tools.RevisionError.Mismatch) {
+      return c.json({ message: "revision.mismatch", _known: true }, 412);
+    }
+
+    if (error.message === bg.Preferences.VO.SupportedLanguagesSetError.Missing) {
+      return c.json({ message: "unsupported.language", _known: true }, 400);
+    }
+
+    if (error.message === tools.DateRangeError.Invalid) {
+      return c.json({ message: "invalid.date.range", _known: true }, 400);
+    }
+
+    if (error instanceof z.ZodError) {
+      Logger.error({
+        message: "Invalid payload",
+        component: "http",
+        operation: "invalid_payload",
+        correlationId,
+        metadata: { url, body: await bg.safeParseBody(c) },
+        error: bg.formatError(error),
+      });
+
+      return c.json({ message: "payload.invalid.error", _known: true }, 400);
+    }
+
+    Logger.error({
+      message: "Unknown error",
+      component: "http",
+      operation: "unknown_error",
+      correlationId,
+      error: bg.formatError(error),
+    });
+
+    return c.json({ message: "general.unknown" }, 500);
+  };
+}
