38529329ea8078f3c1aa3c0646bd8984b838d6f6
Bartosz Gordon <gordonbartosz@gmail.com>
2025-11-03 19:05:24 +0100

    step: add infra/index

---
+++ infra/index.ts
@@ -0,0 +1,16 @@
+import type * as bg from "@bgord/bun";
+import * as tools from "@bgord/tools";
+import { HTTPException } from "hono/http-exception";
+import type { TimingVariables } from "hono/timing";
+
+export const requestTimeoutError = new HTTPException(408, {
+  message: "request_timeout_error",
+});
+
+type Variables = TimingVariables & bg.TimeZoneOffsetVariables & bg.ContextVariables & bg.EtagVariables;
+
+export type HonoConfig = { Variables: Variables; startup: tools.Stopwatch };
+
+export const BODY_LIMIT_MAX_SIZE = tools.Size.fromKb(128).toBytes();
+
+export const IDLE_TIMEOUT = tools.Duration.Seconds(10).seconds;
