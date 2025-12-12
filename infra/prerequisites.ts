87eaea53e1fae9641fbb4219d1ca56153e907aac
Bartosz Gordon <gordonbartosz@gmail.com>
2025-12-12 19:35:15 +0100

    refactor: adjust prereqs

--- infra/prerequisites.ts
+++ infra/prerequisites.ts
@@ -1,45 +1,54 @@
 import * as bg from "@bgord/bun";
 import * as tools from "@bgord/tools";
-import * as Adapters from "+infra/adapters";
-import { Env } from "+infra/env";
+import type { EnvironmentType } from "+infra/env";
 
-const production = Env.type === bg.NodeEnvironmentEnum.production;
+type Dependencies = {
+  DiskSpaceChecker: bg.DiskSpaceCheckerPort;
+  Logger: bg.LoggerPort;
+  Mailer: bg.MailerPort;
+  CertificateInspector: bg.CertificateInspectorPort;
+  Timekeeper: bg.TimekeeperPort;
+};
 
-export const prerequisites = [
-  new bg.PrerequisitePort({ label: "port", port: Env.PORT }),
-  new bg.PrerequisiteTimezoneUTC({ label: "timezone", timezone: tools.Timezone.parse(Env.TZ) }),
-  new bg.PrerequisiteRAM({ label: "RAM", minimum: tools.Size.fromMB(128), enabled: production }),
-  new bg.PrerequisiteSpace({
-    label: "disk-space",
-    minimum: tools.Size.fromMB(512),
-    DiskSpaceChecker: Adapters.DiskSpaceChecker,
-  }),
-  new bg.PrerequisiteNode({
-    label: "node",
-    version: tools.PackageVersion.fromString("24.3.0"),
-    current: process.version,
-  }),
-  new bg.PrerequisiteBun({
-    label: "bun",
-    version: tools.PackageVersion.fromString("1.3.3"),
-    current: Bun.version,
-  }),
-  new bg.PrerequisiteMemory({ label: "memory-consumption", maximum: tools.Size.fromMB(300) }),
-  new bg.PrerequisiteLogFile({ label: "log-file", Logger: Adapters.Logger, enabled: production }),
-  new bg.PrerequisiteOutsideConnectivity({ label: "outside-connectivity", enabled: production }),
-  new bg.PrerequisiteRunningUser({ label: "user", username: "bgord", enabled: production }),
-  new bg.PrerequisiteSSLCertificateExpiry({
-    label: "ssl",
-    hostname: "homepage.bgord.dev",
-    days: 7,
-    enabled: production,
-    CertificateInspector: Adapters.CertificateInspector,
-  }),
-  new bg.PrerequisiteClockDrift({
-    label: "clock-drift",
-    enabled: production,
-    skew: tools.Duration.Minutes(1),
-    Timekeeper: Adapters.Timekeeper,
-  }),
-  new bg.PrerequisiteOs({ label: "os", accepted: ["Darwin", "Linux"] }),
-];
+export function createPrerequisites(Env: EnvironmentType, deps: Dependencies) {
+  const production = Env.type === bg.NodeEnvironmentEnum.production;
+
+  return [
+    new bg.PrerequisitePort({ label: "port", port: Env.PORT }),
+    new bg.PrerequisiteTimezoneUTC({ label: "timezone", timezone: tools.Timezone.parse(Env.TZ) }),
+    new bg.PrerequisiteRAM({ label: "RAM", minimum: tools.Size.fromMB(128), enabled: production }),
+    new bg.PrerequisiteSpace({
+      label: "disk-space",
+      minimum: tools.Size.fromMB(512),
+      DiskSpaceChecker: deps.DiskSpaceChecker,
+    }),
+    new bg.PrerequisiteNode({
+      label: "node",
+      version: tools.PackageVersion.fromString("24.1.0"),
+      current: process.version,
+    }),
+    new bg.PrerequisiteBun({
+      label: "bun",
+      version: tools.PackageVersion.fromString("1.3.4"),
+      current: Bun.version,
+    }),
+    new bg.PrerequisiteMemory({ label: "memory-consumption", maximum: tools.Size.fromMB(300) }),
+    new bg.PrerequisiteLogFile({ label: "log-file", Logger: deps.Logger, enabled: production }),
+    new bg.PrerequisiteMailer({ label: "mailer", enabled: production, Mailer: deps.Mailer }),
+    new bg.PrerequisiteOutsideConnectivity({ label: "outside-connectivity", enabled: production }),
+    new bg.PrerequisiteRunningUser({ label: "user", username: "bgord", enabled: production }),
+    new bg.PrerequisiteSSLCertificateExpiry({
+      label: "ssl",
+      hostname: "homepage.bgord.dev",
+      days: 7,
+      CertificateInspector: deps.CertificateInspector,
+      enabled: production,
+    }),
+    new bg.PrerequisiteClockDrift({
+      label: "clock-drift",
+      skew: tools.Duration.Minutes(1),
+      Timekeeper: deps.Timekeeper,
+      enabled: production,
+    }),
+  ];
+}
